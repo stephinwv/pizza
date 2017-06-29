658abbf46e1dd5d27a1ebbb850f3ed4a0c1e37fb
stephinwv <thekingdomseeker@aol.com>
2017-06-29 19:54:13 -0400

    Add files via upload

--- pizza.rb
+++ pizza.rb
@@ -24,16 +24,35 @@ end
 
 pizza = 0
 x = How_Many?
+count = 1
+cost = 0
 
 x.times do []
-
 pizza += 1
-puts "Pizza " + pizza.to_s
+p "Pizza #{count}"
+	count +=1
 puts size
 puts crust
 puts sauce
 puts cheese
 puts veggies
 puts meats
+	if size == "personal"
+		cost += 4
+		
+		elsif size == "medium"
+			cost += 6
+		
+		elsif size == "large"
+			cost += 9
+		
+		elsif size == "ex large"
+			cost += 13
+		
+	end	
 end
+puts "Total is $#{cost + cost}"
+
+
+
 	
